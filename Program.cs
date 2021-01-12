fb874ea202a99a712e58fbc809f9d7badb4d1e0e
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-12 12:12:58 +0200

    task1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FundametalsCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PrintNumber(int.Parse(Console.ReadLine()));
+
+        }
+        
+        static void PrintNumber(int num) 
+        {
+            
+            if (num > 0)
+            {
+                Console.WriteLine($"the number {num} is positive");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"the number {num} is negative ");
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine($"the number {num} is zero ");
+            }
+            
+        }
+    }
+}
