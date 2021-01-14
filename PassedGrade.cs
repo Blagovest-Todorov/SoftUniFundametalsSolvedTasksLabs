a005401c536cf986e0635a55eeb8617bd94ae195
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-14 09:35:28 +0200

    add file PassedGrade

---
+++ PassedGrade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Passed
+{
+    class PassedGrade
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3)
+            {
+                 Console.WriteLine("Passed!");
+            }
+            
+        }
+    }
+}
